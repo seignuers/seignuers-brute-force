92f365808fd0e2d2be99042adfc14cdb4a66ecf6
seignuers <111926950+seignuers@users.noreply.github.com>
2022-08-24 04:07:11 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+from os import name , system
+from requests import post
+from colorama import Fore
+from datetime import datetime
+from uuid import uuid4
+from time import sleep
+import json
+import webbrowser
+red = Fore.RED
+green = Fore.GREEN
+blue = Fore.BLUE
+white = Fore.WHITE
+if name == 'nt':
+    system('cls')
+    webbrowser.open('https://t.me/NullCyberi')
+else:
+    system('clear')
+    system('xdg-open https://t.me/NullCyberi')
+print(white+'''
+                                 
+          
